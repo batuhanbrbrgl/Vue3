51502cdf42229dbb3ae88a376585ee52f397e0b5
Batuhan BERBEROĞLU <bthnnapcan@gmail.com>
2023-02-24 20:12:17 +0300

    vue-router

---
+++ vue3-firebase/vue-router/src/views/Personel.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Personeller</h1>
+  <div v-for="personel in personeller" :key="personel.id">
+    <p>{{personel.ad}}</p>
+  </div>
+  
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            personeller:[
+                {id:1,ad: Luffy, gorev:kaptan},
+                {id:2,ad: Zoro, gorev:Yardımcı},
+                {id:3,ad: Sanji, gorev:Aşcı},
+
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
